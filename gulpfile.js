dc895a376c77f64f15c079d91b7d1af791f2462f
dominus-v <a.swan75@gmail.com>
2018-09-30 02:07:05 +0300

    Added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var	gulp = require('gulp'),
 	watch = require('gulp-watch'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer'),
-	cssvars = require('postcss-simple-vars'),
+	postCSS = require('gulp-postcss'),
+	autoPrefixer = require('autoprefixer'),
+	cssVars = require('postcss-simple-vars'),
 	nested = require('postcss-nested'),
-	cssimport = require('postcss-import');
+	cssImport = require('postcss-import'),
+	browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
 	console.log('Hey, a lot of tasks to be solved! Go ahead!');
@@ -16,16 +17,36 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssimport, autoprefixer, nested, cssvars]))
+		.pipe(postCSS([cssImport, autoPrefixer, nested, cssVars]))
 		.pipe(gulp.dest('./app/temp/styles/'));
-})
+});
 
 gulp.task('watch', function() {
+	// инициализируем объект сервер синхронизации с браузером, при этом
+	// в качестве аргумента сервера передается путь директории, в которой
+	// находится приложение, запускаемое в браузере
+	browserSync.init({
+		host: '192.168.1.43',
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
 	watch('./app/index.html', function() {
-		gulp.start('html');
+		browserSync.reload();
 	});
 
 	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	})
-})
+		gulp.start('cssInject');
+	});
+});
+
+// задача для отправки файла стилей в браузер
+// перед отправкой, выполняется задача CSS-препроцессинга 'styles'
+gulp.task('cssInject', ['styles'], function(){
+	// отправляем файл стилей 'styles.css' в поток browserSync
+	// иными словами, отправляем файл стилей в браузер
+	return gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
+});
